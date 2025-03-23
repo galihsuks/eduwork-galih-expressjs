cd522504463f73bc8435f4eeff0a03381f4022fc
galihsuks <galih8.4.2001@gmail.com>
2025-03-23 09:39:00 +0700

    buat CRUD

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 8083;
+const productRouter = require("./app/product/routes");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Backend Tugas Eduwork");
 });
-app.get("/welcome", (req, res) => {
-    res.status(200).json({
-        pesan: "Selamat Datang di Tugas Eduwork",
-    });
-});
-app.get("/about", (req, res) => {
-    res.status(200).json({
-        pesan: "Ini halaman tentang",
-    });
-});
+
+app.use("/product", productRouter);
 
 app.listen(port, () => {
     console.log(`Backend Absensi app listening on port ${port}`);
